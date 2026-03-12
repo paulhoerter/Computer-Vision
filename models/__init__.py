73fe1bdd3b824ef98c80554b2b92af584652a2d0
paulhoerter <paulhoerter1@gmail.com>
2026-03-12 17:45:17 +0100

    Sync local

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .unet      import UNet
-from .resnet    import ResNet
-from .inception import Inception
-
-MODEL_REGISTRY = {
-    "U-Net":     UNet,
-    "ResNet":    ResNet,
-    "Inception": Inception,
-}
